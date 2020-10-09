56ba4cf451cb3f6fb8f69e6d41ebfd2ef63c6c67
Robert Velasco <robertv3@vt.edu>
2020-10-09 16:00:14 -0400

    added get distance to next target code

--- mirConnection.py
+++ mirConnection.py
@@ -29,6 +29,18 @@ class mirConnection:
         print("Req: ", req)
         print("Status: ", missions)
     
+    #still need to test
+    #GET status, then grab the 'distance to next target' value (in meters)
+    def getDistFromTarget(self):
+        http = urllib3.PoolManager()
+        req = http.request('GET', 'http://192.168.1.8:8080/v2.0.0/status', headers={'Content-Type': 'application/json',
+                'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='})
+        status = json.loads(req.data.decode('utf-8'))
+        distance = status['distance_to_next_target']
+        print("Distance To Target: ", distance)
+        return distance
+    
+    #Still need to test
     def deleteMissions(self):
         http = urllib3.PoolManager()
         req = http.request('DELETE', 'http://192.168.1.8:8080/v2.0.0/mission_queue/', 
