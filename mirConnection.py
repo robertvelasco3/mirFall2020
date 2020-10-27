1fd10929ab5d477e5f38fb5b250974ac07872562
Robert Velasco <robertv3@vt.edu>
2020-10-27 16:12:47 -0400

    adding comments and changes to mirConnection class

--- mirConnection.py
+++ mirConnection.py
@@ -6,17 +6,15 @@ class mirConnection:
         http = urllib3.PoolManager()
         req = http.request('GET', 'http://192.168.1.8:8080/v2.0.0/status', headers={'Content-Type': 'application/json',
                 'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='})
-        missions = json.loads(req.data.decode('utf-8'))
-        print("Req: ", req)
-        print("Status: ", missions)
+        status = json.loads(req.data.decode('utf-8'))
+        print("Status: ", status)
         
     def getMissions(self):
         http = urllib3.PoolManager()
         req = http.request('GET', 'http://192.168.1.8:8080/v2.0.0/missions', headers={'Content-Type': 'application/json',
                 'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='})
         missions = json.loads(req.data.decode('utf-8'))
-        print("Req: ", req)
-        print("Status: ", missions)
+        print("Missions: ", missions)
         
     #Use this method to add a mission to MiR's mission queue, give it the mission's mission_id (constants given in testCode)
     def performMission(self, missionID):
@@ -28,8 +26,12 @@ class mirConnection:
         missions = json.loads(req.data.decode('utf-8'))
         print("Req: ", req)
         print("Status: ", missions)
+        if(req.status == 200):
+            print("Mission Successfully Queued: ", missionID)
+        else:
+            print("Error while queueing mission: ", req.status)
     
-    #GET status, then grab the 'distance to next target' value (in meters)
+    #GET status, then grab the 'distance to next target' value (in meters) and any current errors
     def getDistFromTarget(self):
         http = urllib3.PoolManager()
         req = http.request('GET', 'http://192.168.1.8:8080/v2.0.0/status', headers={'Content-Type': 'application/json',
@@ -42,48 +44,41 @@ class mirConnection:
         print("Errors:", errors)
         return (distance, errors)
     
+    # Clears the error flag from the MiR after an error has occured. In this way, the code can 
+    # keep running even after an error occurs.
     def clearError(self):
         statusBody = json.dumps({"clear_error" : True})
-        print("statusBod", statusBody)
         http = urllib3.PoolManager()
         req = http.request('PUT', 'http://192.168.1.8:8080/v2.0.0/status', headers={'Content-Type': 'application/json',
                 'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='},
                            body=statusBody)
         status = json.loads(req.data.decode('utf-8'))
-        print("___", "*Clear Error*")
-        print("Status:", status)
-        distance = status['distance_to_next_target']
-        errors = status['errors']
-        print("Distance To Target: ", distance)
-        print("Errors:", errors)
+        if(req.status == 200):
+            print("Error cleard.")
+        else:
+            print("Error while clearing error: ", req.status)
     
+    # Sets the state of the MiR's mission queue to 'Ready' - It will automatically change to pause 
+    # after an error occurs, so it needs to be changed back to ready after an error occurs
     def makeReady(self):
         statusBody = json.dumps({"state_id" : 3})
-        print("statusBod", statusBody)
         http = urllib3.PoolManager()
         req = http.request('PUT', 'http://192.168.1.8:8080/v2.0.0/status', headers={'Content-Type': 'application/json',
                 'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='},
                            body=statusBody)
         status = json.loads(req.data.decode('utf-8'))
-        print("___", "*Clear Error*")
-        print("Status:", status)
-        distance = status['distance_to_next_target']
-        errors = status['errors']
-        print("Distance To Target: ", distance)
-        print("Errors:", errors)
+        if(req.status == 200):
+            print("MiR is Ready.")
+        else:
+            print("Error while setting MiR state: ", req.status)
     
-    #Still need to test
+    # Deletes the current missions in the MiR queue
     def deleteMissions(self):
         http = urllib3.PoolManager()
         req = http.request('DELETE', 'http://192.168.1.8:8080/v2.0.0/mission_queue/', 
             headers={'Content-Type': 'application/json', 'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='})
-        print(req)
-
-    #Still need to test
-    def endMission(self, missionID):
-        http = urllib3.PoolManager()
-        req = http.request('DELETE', 'http://192.168.1.8:8080/v2.0.0/mission_queue/' + missionID, 
-            headers={'Content-Type': 'application/json', 'authorization': 'Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='})
-        missions = json.loads(req.data.decode('utf-8'))
-        print("Req: ", req)
-        print("Status: ", missions)
+        #response = json.loads(req.data.decode('utf-8'))
+        if(req.status == 200):
+            print("Misisons Deleted.")
+        else:
+            print("Error while deleting missions: ", req.status)
